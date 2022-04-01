ab4e05e4a5b726b34061ce32ac769624a6d79633
Summer Burgess <94207600+sabsc98@users.noreply.github.com>
2022-03-31 18:41:54 -0600

    Update game.js

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ let availableQuestions = [];
 
 let questions = [
     {
-        question: 'Which best describes the Major Arcana card "The Wheel of Fortune" '
+        question: 'Which best describes the Major Arcana card The Wheel of Fortune',
         choice1: 'Fundamental change is imminent. The positive benefits you gain during this period could last a long time.',
         choice2: 'The law of karma, has to do with moral sensitivity and that which gives rise to empathy, compassion, and a sense of fairness. Since the time of Solomon, this image has represented a standard for the humane and fair-minded treatment of other beings.',
         choice3: 'Freeing yourself from a past that no longer serves you well and proceed toward the future.',
@@ -18,19 +18,19 @@ let questions = [
         answer: 1,
     },
     {
-        question: Which card best describes this statement? 'the culturally rejected wildness and undigested shadow side that each of us carries in our subconscious. This shadow is actually at the core of our being',
-        choice1: The Magician,
-        choice2: Two of Swords,
-        choice3: The Devil,
-        choice4: Ace of Wands,
+        question: 'Which card best describes this statement? the culturally rejected wildness and undigested shadow side that each of us carries in our subconscious. This shadow is actually at the core of our being',
+        choice1: "The Magician",
+        choice2: "Two of Swords",
+        choice3: "The Devil",
+        choice4: "Ace of Wands",
         answer: 3,    
     },
     {
-        question: Which card best describes this statement? 'Representative of the ultimate test of a souls integrity, where the membrane between the self and the unknown is removed, and the drop of individuality re-enters the ocean of being. ',
-        choice1: Eight of cups,
-        choice2: Three of Swords,
-        choice3: Knight of Wands,
-        choice4: The Moon,
+        question: 'Which card best describes this statement? Representative of the ultimate test of a souls integrity, where the membrane between the self and the unknown is removed, and the drop of individuality re-enters the ocean of being.',
+        choice1: "Eight of cups",
+        choice2: "Three of Swords",
+        choice3: "Knight of Wands",
+        choice4: "The Moon",
         answer: 4,
     },
 ];
@@ -43,35 +43,37 @@ startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
+    console.log(availableQuestions);
     getNewQuestion();
 };
 
 getNewQuestion = () => {
-    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
         //go to the end page 
         return window.location.assign('/end.html');
     }
     questionCounter++;
-    const questionIndex = Math.floor(Math.random() * availableeQuestions.length);
-    currentQuestion = availableQuestions[questionIndex, 1];
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
 
     choices.forEach((choice) => {
-        const number = choice.dataset['number'];
+        const number = choice.dataset["number"];
         choice.innerText = currentQuestion['choice' + number];
     });
 
     availableQuestions.splice(questionIndex, 1);
+    console.log(availableQuestions);
     acceptingAnswers = true;
 };
 
 choices.forEach((choice) => {
-    choice.addEventListener('click', (e) => {
+    choice.addEventListener("click", e => {
         if (!acceptingAnswers) return;
 
         acceptingAnswers = false;
         const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset['number'];
+        const selectedAnswer = selectedChoice.dataset["number"];
         getNewQuestion();
     });
 });
